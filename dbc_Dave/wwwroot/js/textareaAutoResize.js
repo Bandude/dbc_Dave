aa377129e4c3931d12386a0204d007551ce4d6b3
Craig Jones <craig@designbycraig.com>
2023-09-01 01:40:40 -0500

    Added main index.razor still need to seperate out code and html

---
+++ dbc_Dave/wwwroot/js/textareaAutoResize.js
@@ -0,0 +1,67 @@
+window.resizeTextArea = (id) => {
+    element = document.getElementById(id);
+    element.style.height = (element.scrollHeight) + "px";
+    element.style.overflow = "hidden";
+    element.style.resize = "none";
+};
+
+function highlightAllCodeBlocks() {
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightElement(block);
+    });
+}
+
+function  resetMessageBox() {
+    const messageBox = document.getElementById('newmessage');
+    messageBox.style.height = "80px";
+    console.log("Reset Message Box");
+    setTimeout(function () {
+        scrollToBottom();
+        if (messageBox) {
+            element.focus();
+        }
+    }, 200);
+
+}
+
+
+window.setFocusOnElement = function (id) {
+    var element = document.getElementById(id);
+    if (element) {
+        element.focus();
+    }
+}
+
+
+function handleButtonClick(event, codeBlockId) {
+    event.stopPropagation();
+    var preTag = document.querySelector(`code[id='codeBlock${codeBlockId}']`);
+    var textToCopy = preTag.textContent || preTag.innerText;
+
+    // Creating a temporary textarea to copy the text
+    var tempTextarea = document.createElement('textarea');
+    tempTextarea.value = textToCopy;
+    document.body.appendChild(tempTextarea);
+    tempTextarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempTextarea);
+
+
+}
+
+
+
+function scrollToBottom() {
+    window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth'
+    });
+
+}
+
+window.setFocusOnElement = function (id) {
+    var element = document.getElementById(id);
+    if (element) {
+        element.focus();
+    }
+};
